9b657e148b03273dfdc493861f4837e49fb7ef05
Aluno UNIPAR <aluno@ac-cas.unipar.br>
2024-08-13 22:12:52 -0300

    testes

--- Clinica/src/main/java/com/unipar/clinicapp/Controller/Login.java
+++ Clinica/src/main/java/com/unipar/clinicapp/Controller/Login.java
@@ -20,27 +20,10 @@ public class Login {
         Usuario usuario = usuarioService.validarUsuario(username, password);
         if (usuario != null) {
             session.setAttribute("UsuarioLogado", username);
-            return "redirect:/view/paginaInicial";
+            return "redirect:/paginaInicial.html";
         } else {
             model.addAttribute("erro", "Usuário ou Senha inválidos!");
-            return "login";
-        }
-    }
-
-    @PostMapping("/cadastrarUsuarios")
-    public String cadastrarUsuarios(@RequestParam("nome") String nome, @RequestParam("login") String login,
-                                    @RequestParam("senha") String senha, @RequestParam("confirmarSenha") String confirmarSenha,
-                                    @RequestParam("telefone") String telefone, Model model) {
-        if (senha.equals(confirmarSenha)) {
-            Usuario novoUsuario = new Usuario();
-            novoUsuario.setNome(nome);
-            novoUsuario.setLogin(login);
-            novoUsuario.setSenha(senha);
-            usuarioService.salvarUsuario(novoUsuario);
-            return "redirect:/view/paginaInicial";
-        } else {
-            model.addAttribute("erro", "As senhas não coincidem!");
-            return "cadastro";
+            return "redirect:/index.html";
         }
     }
 }
--- Clinica/src/main/resources/static/js/index.js
+++ Clinica/src/main/resources/static/js/index.js
@@ -1,5 +1,4 @@
 
     $("#btCadastrar").on("click", function(){
         $("#cadastroModal").modal("show");
-        console.log("essa porra vai quebrar tem que arrumar e eu nao vou fazer isso ):")
     });
