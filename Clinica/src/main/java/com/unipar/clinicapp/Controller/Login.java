2bce82375035ba17204dacf05041662f9cdb064e
FelipeKojiM <felipekoji1@outlook.com>
2024-08-13 23:08:43 -0300

    teste

--- Clinica/src/main/java/com/unipar/clinicapp/Controller/Login.java
+++ Clinica/src/main/java/com/unipar/clinicapp/Controller/Login.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,15 +16,23 @@ public class Login {
     @Autowired
     UsuarioService usuarioService;
 
-    @PostMapping("/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model) {
-        Usuario usuario = usuarioService.validarUsuario(username, password);
-        if (usuario != null) {
-            session.setAttribute("UsuarioLogado", username);
-            return "redirect:/paginaInicial.html";
-        } else {
-            model.addAttribute("erro", "Usuário ou Senha inválidos!");
-            return "redirect:/index.html";
-        }
+    @PostMapping("/loginUsuario")
+    public String processarLogin(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model) {
+        // Lógica para processar login
+        // Adicione a lógica para verificar as credenciais e retornar a página apropriada
+        return "paginaInicial"; // Ou redirecione conforme necessário
     }
+
+
+//    @PostMapping("/login")
+//    public String login(@RequestParam("username") String username, @RequestParam("password") String password, HttpSession session, Model model) {
+//        Usuario usuario = usuarioService.validarUsuario(username, password);
+//        if (usuario != null) {
+//            session.setAttribute("UsuarioLogado", username);
+//            return "redirect:/paginaInicial";
+//        } else {
+//            model.addAttribute("erro", "Usuário ou Senha inválidos!");
+//            return "redirect:/paginaInicial";
+//        }
+//    }
 }
