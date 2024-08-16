e8d32eda27e7e53f6ed544e8352102af09fcb72e
Matheus S <theus.schmidt@hotmail.com>
2024-08-16 16:32:44 -0300

    Isso ai

---
+++ Clinica/src/main/java/com/unipar/clinicapp/Controller/PacienteWebController.java
@@ -0,0 +1,37 @@
+package com.unipar.clinicapp.Controller;
+
+import com.unipar.clinicapp.Model.Paciente;
+import com.unipar.clinicapp.Service.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class PacienteWebController {
+
+    private final PacienteService pacienteService;
+
+    public PacienteWebController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+    @Operation(summary = "Obter todos os pacientes", description = "Retorna uma lista de todos os pacientes cadastrados.")
+    @GetMapping(path = "/cadastroPaciente")
+    public String getAllPaciente(Model model) {
+        List<Paciente> paciente = pacienteService.getAll();
+        model.addAttribute("paciente", paciente);
+        return "paciente";
+    }
+
+    @Operation(summary = "Salvar um paciente", description = "Salva um novo paciente no sistema.")
+    @PostMapping(path = "/paciente/save")
+    public String savePaciente(Paciente paciente) {
+        pacienteService.save(paciente);
+        return "redirect:/pacientes";
+    }
+
+}
